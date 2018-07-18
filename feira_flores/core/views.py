b4e0d2bc25c1c00787caf6a9dea468a178277bbf
alvesgabriel <gabriel.alves.monteiro1@gmail.com>
2018-07-17 22:05:56 -0300

    Criada home page usando o Twitter Bootstrap
    close #20

--- feira_flores/core/views.py
+++ feira_flores/core/views.py
@@ -1,6 +1,5 @@
-from django.http import HttpResponse
-# from django.shortcuts import render
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('Olá Mundo')
+    return render(request, 'home.html')
